7c15b0177669d8b08e47e84c2f2035c1c7fae3f7
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2025-11-23 22:06:15 +0500

    feat: added booking dtos

---
+++ Autorent.Application/DTO/Booking/BookingCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Autorent.Application.DTO.Booking
+{
+    public class BookingCreateDto
+    {
+        public int CarId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
