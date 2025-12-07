5a6689a9f24232958ccfbac00b38dfdd10de784f
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2025-12-08 00:08:41 +0500

    chore: Add extensions

---
+++ Autorent.Infrastructure/Extensions/CorsExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Autorent.Infrastructure.Extensions
+{
+    public static class CorsExtensions
+    {
+        public static void AddCorsConfiguration(this WebApplicationBuilder builder)
+        {
+            var allowedOrigins = builder.Configuration
+                .GetSection("Cors:AllowedOrigins")
+                .Get<string[]>()
+                ?? throw new Exception("Cors:AllowedOrigins is Missing");
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy
+                        .WithOrigins(allowedOrigins)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+        }
+    }
+}
---
+++ Autorent.Infrastructure/Extensions/SeederExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Autorent.Infrastructure.Persistence;
+using Autorent.Infrastructure.Persistence.Seeders;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Autorent.Infrastructure.Extensions
+{
+    public static class SeederExtensions
+    {
+        public static async Task UseSeeders(this WebApplication app)
+        {
+            if (!app.Environment.IsDevelopment())
+                return;
+
+            using var scope = app.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            Console.WriteLine("🌱 Running seeders (Development only)...");
+            await SeedData.Initialize(db);
+        }
+    }
+}
